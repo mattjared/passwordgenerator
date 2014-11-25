cca990d3993e1915fd7c8d7324968bce9089cde1
Matt Jared <mattjared9@gmail.com>
2014-11-25 08:35:52 -0600

    Adding logic to attach password to an h1 with styles and adding jQuery / js properly

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-// function (passGen) {
+// // function (passGen) {
 	var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*";
-// };
+// // };
 
-
-$(".result").append(characters);
-$()
-
-
-Potential way to add classes immediately after appending var = "characters" to a div
-// $(this).after( $("<div></div>").addClass(myClass) );
+$(".h1").append(characters);
